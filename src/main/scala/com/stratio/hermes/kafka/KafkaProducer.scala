3ee5ec1e296190260f1e38abb1ee7e9fcd621eeb
eruizgar <eruiz@stratio.com>
2016-11-28 16:43:31 +0100

    Add Kafka producer

--- src/main/scala/com/stratio/hermes/kafka/KafkaProducer.scala
+++ src/main/scala/com/stratio/hermes/kafka/KafkaProducer.scala
@@ -1,28 +1,55 @@
+/*
+ * Copyright (C) 2016 Stratio (http://stratio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.stratio.hermes.kafka
 
 import java.util.Properties
 
 import com.typesafe.config.Config
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.slf4j.LoggerFactory
 
 
 object KafkaProducer {
 
+  val log = LoggerFactory.getLogger(getClass)
 
-  def getInstance(config: Config) : KafkaProducer[AnyRef, AnyRef]= {
+  def getInstance(config: Config): KafkaProducer[AnyRef, AnyRef] = {
     val props: Properties = new Properties()
     props.put("metadata.broker.list", config.getString("brokerList"))
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     props.put("bootstrap.servers", config.getString("servers"))
-    props.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     props.put("request.requieres.acks", "1")
     new KafkaProducer(props)
   }
 
   def send(producer: KafkaProducer[AnyRef, AnyRef], topic: String, message: String): Unit = {
-    val record = new ProducerRecord[AnyRef, AnyRef](topic, message)
-    producer.send(record)
-  }
-  def close(producer: KafkaProducer[AnyRef, AnyRef]): Unit = producer.close()
+    try {
+      log.info("Sending message : [" + message + "] to the topic: " + topic)
+      producer.send(new ProducerRecord[AnyRef, AnyRef](topic, message))
 
+    } catch {
+      case e: Exception =>
+        log.error("Error sending message: [" + message + "]")
+        log.error("Exception: " + e.getMessage)
+    }
+  }
+
+  def close(producer: KafkaProducer[AnyRef, AnyRef]): Unit = {
+    log.info("Producer closed.")
+    producer.close()
+  }
 }
--- src/test/scala/com/stratio/hermes/kafka/KafkaProducerTest.scala
+++ src/test/scala/com/stratio/hermes/kafka/KafkaProducerTest.scala
@@ -1,7 +1,23 @@
+/*
+ * Copyright (C) 2016 Stratio (http://stratio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.stratio.hermes.kafka
 
-import java.util.Properties
 import java.util
+import java.util.Properties
 
 import com.typesafe.config.ConfigFactory
 import org.apache.kafka.clients.consumer.KafkaConsumer
@@ -9,43 +25,47 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FlatSpec, Matchers}
 
-
+import scala.io.Source
+import scala.util.Try
 
 
 @RunWith(classOf[JUnitRunner])
 class KafkaProducerTest extends FlatSpec with Matchers {
 
+  val poll_time = 1000
+  val kafka_host = System.getProperty("KAFKA_HOST", "localhost")
+  val kafka_port = System.getProperty("KAFKA_PORT", "9092")
+  val topicName = "test"
+
   val props = new Properties
-    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-    props.put("bootstrap.servers", "localhost:9092")
-    props.put("enable.auto.commit", "true")
-    props.put("group.id", "0")
-    props.put("auto.commit.interval.ms", "1000")
-    props.put("session.timeout.ms", "30000")
+  props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("bootstrap.servers", kafka_host + ":" + kafka_port)
+  props.put("enable.auto.commit", "true")
+  props.put("group.id", "0")
+  props.put("auto.commit.interval.ms", "1000")
+  props.put("session.timeout.ms", "30000")
 
-  "A KafkaProducer" should "Produce a message with a json" in {
+  val producer_test = Try(getClass.getResourceAsStream(s"/producer_test.json"))
+    .getOrElse(throw new IllegalStateException(s"Error loading locale: /producer_test.json"))
 
-    val kafkaProducer=KafkaProducer.getInstance(ConfigFactory.parseResources("kafka.conf"))
 
+  "A KafkaProducer" should "Produce a message and be consumed" in {
 
-//    for (line <- Source.fromFile("/home/eruiz/Proyectos/Hermes/src/test/resources/test").getLines){
+    val kafkaProducer = KafkaProducer.getInstance(ConfigFactory.parseResources("kafka.conf"))
+
     val consumer = new KafkaConsumer(props)
-    consumer.subscribe(util.Arrays.asList("test"))
+    consumer.subscribe(util.Arrays.asList(topicName))
 
-    val records1 = consumer.poll(1000)
+    val records1 = consumer.poll(poll_time)
     records1.count() shouldEqual 0
 
-    KafkaProducer.send(kafkaProducer,"test","{\"name\":\"amparo\"}")
-    val records2 = consumer.poll(1000)
+    KafkaProducer.send(kafkaProducer, topicName, Source.fromInputStream(producer_test).mkString)
+
+    val records2 = consumer.poll(poll_time)
     records2.count() shouldEqual 1
 
-    kafkaProducer.close()
+    KafkaProducer.close(kafkaProducer)
     consumer.close()
-
-
-    //    KafkaProducer.send(kafkaProducer,"topic2",Source.fromFile("/home/eruiz/Proyectos/Hermes/src/test/resources/test").getLines())
-
-
   }
 }
