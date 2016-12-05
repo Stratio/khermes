77677110a670fbe727d23a5aa2edbe763734da16
eruizgar <eruiz@stratio.com>
2016-12-05 15:25:38 +0100

    Fix space in contain test

--- src/test/scala/com/stratio/hermes/utils/HermesTest.scala
+++ src/test/scala/com/stratio/hermes/utils/HermesTest.scala
@@ -24,44 +24,44 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FlatSpec, Matchers}
 
 @RunWith(classOf[JUnitRunner])
-class HermesTest extends FlatSpec with Matchers with NumberSign{
+class HermesTest extends FlatSpec with Matchers {
 
   "A Hermes" should "generates random firstNames and lastNames with EN and ES locales" in {
     val hermesEN = Hermes()
-    hermesEN.Name.nameModel.firstNames should contain(hermesEN.Name.firstName)
-    hermesEN.Name.nameModel.lastNames should contain(hermesEN.Name.lastName)
+    hermesEN.Name.nameModel.firstNames should contain (hermesEN.Name.firstName)
+    hermesEN.Name.nameModel.lastNames should contain (hermesEN.Name.lastName)
 
     val hermesES = Hermes("ES")
-    hermesES.Name.nameModel.firstNames should contain(hermesES.Name.firstName)
-    hermesES.Name.nameModel.lastNames should contain(hermesES.Name.lastName)
+    hermesES.Name.nameModel.firstNames should contain (hermesES.Name.firstName)
+    hermesES.Name.nameModel.lastNames should contain (hermesES.Name.lastName)
   }
 
   it should "generate valid names: firstName lastName with EN and ES locales" in {
     val hermesEN = Hermes()
     val fullNameEN = hermesEN.Name.fullName
     fullNameEN should fullyMatch regex """[a-zA-Z]+ [a-zA-Z]+"""
-    hermesEN.Name.nameModel.firstNames should contain(fullNameEN.split(" ")(0))
-    hermesEN.Name.nameModel.lastNames should contain(fullNameEN.split(" ")(1))
+    hermesEN.Name.nameModel.firstNames should contain (fullNameEN.split(" ")(0))
+    hermesEN.Name.nameModel.lastNames should contain (fullNameEN.split(" ")(1))
 
     val hermesES = Hermes("ES")
     val fullNameES = hermesES.Name.fullName
     fullNameES should fullyMatch regex """[a-zA-Z]+ [a-zA-Z]+"""
-    hermesES.Name.nameModel.firstNames should contain(fullNameES.split(" ")(0))
-    hermesES.Name.nameModel.lastNames should contain(fullNameES.split(" ")(1))
+    hermesES.Name.nameModel.firstNames should contain (fullNameES.split(" ")(0))
+    hermesES.Name.nameModel.lastNames should contain (fullNameES.split(" ")(1))
   }
 
   it should "generate valid middle names: firstName firstName with EN and ES locales" in {
     val hermesEN = Hermes()
     val middleNameEN = hermesEN.Name.middleName
     middleNameEN should fullyMatch regex """[a-zA-Z]+ [a-zA-Z]+"""
-    hermesEN.Name.nameModel.firstNames should contain(middleNameEN.split(" ")(0))
-    hermesEN.Name.nameModel.firstNames should contain(middleNameEN.split(" ")(1))
+    hermesEN.Name.nameModel.firstNames should contain (middleNameEN.split(" ")(0))
+    hermesEN.Name.nameModel.firstNames should contain (middleNameEN.split(" ")(1))
 
     val hermesES = Hermes("ES")
     val middleNameES = hermesES.Name.middleName
     middleNameES should fullyMatch regex """[a-zA-Z]+ [a-zA-Z]+"""
-    hermesES.Name.nameModel.firstNames should contain(middleNameES.split(" ")(0))
-    hermesES.Name.nameModel.firstNames should contain(middleNameES.split(" ")(1))
+    hermesES.Name.nameModel.firstNames should contain (middleNameES.split(" ")(0))
+    hermesES.Name.nameModel.firstNames should contain (middleNameES.split(" ")(1))
   }
 
   it should "raise an exception when it gets a firstName/lastName and firstNames/lastNames are empty in the locale" in {
