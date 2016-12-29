83861f5e998dfe82438e848f0330117e154f6fd6
Alicia Doblas <adoblasalaez@gmail.com>
2016-12-29 12:30:06 +0100

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -13,8 +13,10 @@ fi
 
 if [[ ! -z ${HOSTNAME} ]]; then
   PARAMS="${PARAMS} -Dakka.remote.netty.tcp.hostname=${HOSTNAME}"
-  sleep 5
-  ping -c2 ${HOSTNAME} &> /dev/null
+  sleep 10
+  #DEMO
+  ping -c10 ${HOSTNAME} 
+  host -t a ${HOSTNAME}
 fi
 
 if [[ ! -z  ${KAFKA_BOOTSTRAP_SERVERS} ]]; then
