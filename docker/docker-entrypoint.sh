9a98d24a4e7a23345c9cdb03144974e3f356cad8
antonioalf <aalfonso@stratio.com>
2017-03-02 10:52:13 +0100

    fix problem with name resolution in DCOS

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,6 +8,12 @@ fi
 client=$(echo $PARAMS | grep hermes.client=true || true)
 echo "Params: ${PARAMS}"
 
+if [[ ! -z ${MARATHON_APP_LABEL_DCOS_PACKAGE_NAME} ]]; then
+	sleep 10
+	ping -c10 ${MARATHON_APP_LABEL_DCOS_PACKAGE_NAME}.marathon.mesos 
+	host -t a ${MARATHON_APP_LABEL_DCOS_PACKAGE_NAME}.marathon.mesos
+fi
+
 if [[ -z ${client} ]]; then
     java -jar ${PARAMS} /hermes.jar
 else
