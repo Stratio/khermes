62b3cd63ec95a57867bff87e0c1243d0e64f3148
arodriguez <arodriguez@arodriguez.stratio>
2017-04-25 17:37:28 +0200

    Modify entry point to separate executions between seed and nodes. Pick up ports and ips from ENV variables and work out SEED_IP_ADDRESS from bash command

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -xe
 
-java -jar -Dkhermes.client=false -Dakka.remote.hostname=localhost -Dakka.remote.netty.tcp.port=2553 -Dakka.cluster.seed-nodes.0=akka.tcp://khermes@localhost:2552 /khermes.jar
+if [ $SEED = "true" ]; then
+    SEED_IP_ADDRESS="$(/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{ print $1}')"
+    java -jar -Dkhermes.ws=true -Dakka.remote.netty.tcp.port=$PORT0 -Dakka.remote.netty.tcp.hostname=$SEED_IP_ADDRESS -Dakka.cluster.seed-nodes.0=akka.tcp://khermes@$SEED_IP_ADDRESS:$PORT0 -Dzookeeper.connection=master.mesos:2181 /khermes.jar
+else
+    java -jar -Dkhermes.client=true -Dkhermes.ws=false -Dakka.remote.netty.tcp.port=$PORT0 -Dakka.cluster.seed-nodes.0=akka.tcp://khermes@$SEED_IP:$SEED_PORT -Dzookeeper.connection=master.mesos:2181 /khermes.jar
+fi
 
 tail -F /khermes.log
-
+s
