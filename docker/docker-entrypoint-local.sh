81c6c69a107357658c1455dc00a4a5d5ef54381e
arodriguez <arodriguez@arodriguez.stratio>
2017-04-28 08:39:16 +0200

    Docker-entrypoint local

--- docker/docker-entrypoint-local.sh
+++ docker/docker-entrypoint-local.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -xe
 
-if [ $SEED = "true" ]; then
-    java -jar -Dkhermes.ws=true -Dakka.remote.netty.tcp.port=2552 -Dakka.remote.netty.tcp.hostname=localhost -Dakka.cluster.seed-nodes.0=akka.tcp://khermes@localhost:2552 -Dzookeeper.connection=localhost:2181 /khermes.jar
-else
-    java -jar -Dkhermes.client=true -Dkhermes.ws=false -Dakka.remote.netty.tcp.port=2553 -Dakka.cluster.seed-nodes.0=akka.tcp://khermes@localhost:2552 -Dzookeeper.connection=localhost:2181 /khermes.jar
-fi
+java -jar -Dkhermes.ws=true -Dakka.remote.netty.tcp.port=2552 -Dakka.remote.netty.tcp.hostname=localhost -Dakka.cluster.seed-nodes.0=akka.tcp://khermes@localhost:2552 -Dzookeeper.connection=localhost:2181 /khermes.jar
 
 tail -F /khermes.log
