eddb1c5add5d9b52bc276f83bf61f1a86640dd69
arodriguez <arodriguez@arodriguez.stratio>
2017-05-04 08:35:24 +0200

    Modify master.mesos to localhost

--- docker/docker-entrypoint-local.sh
+++ docker/docker-entrypoint-local.sh
@@ -3,7 +3,7 @@
 if [ $SEED = "true" ]; then
     java -jar -Dkhermes.ws=true -Dakka.remote.netty.tcp.port=$SEED_PORT -Dakka.remote.netty.tcp.hostname=localhost -Dakka.cluster.seed-nodes.0=akka.tcp://khermes@localhost:$SEED_PORT -Dzookeeper.connection=localhost:$ZK_PORT /khermes.jar
 else
-    java -jar -Dkhermes.client=true -Dkhermes.ws=false -Dakka.remote.netty.tcp.port=$NODE_PORT -Dakka.cluster.seed-nodes.0=akka.tcp://khermes@localhost:$SEED_PORT -Dzookeeper.connection=master.mesos:$ZK_PORT /khermes.jar
+    java -jar -Dkhermes.client=true -Dkhermes.ws=false -Dakka.remote.netty.tcp.port=$NODE_PORT -Dakka.cluster.seed-nodes.0=akka.tcp://khermes@localhost:$SEED_PORT -Dzookeeper.connection=localhost:$ZK_PORT /khermes.jar
 fi
 
 tail -F /khermes.log
