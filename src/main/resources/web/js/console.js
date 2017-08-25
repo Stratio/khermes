4c236340495bd77b7bf5085486f550f93a98e70d
Jose Núñez <jcnunez@stratio.com>
2017-08-25 12:58:50 +0200

    update license

--- src/main/resources/web/js/console.js
+++ src/main/resources/web/js/console.js
@@ -1,3 +1,13 @@
+/*
+ * © 2017 Stratio Big Data Inc., Sucursal en España.
+ *
+ * This software is licensed under the Apache 2.0.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the terms of the License for more details.
+ *
+ * SPDX-License-Identifier:  Apache-2.0.
+ */
 jQuery(document).ready(function($) {
     $('body').terminal(function(command, term) {
         if (command == 'help') {
