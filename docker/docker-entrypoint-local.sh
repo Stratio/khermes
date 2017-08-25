88a839693b4b3ad9aef27422d4daf9115e41d069
Jose Núñez <jcnunez@stratio.com>
2017-08-25 14:24:37 +0200

    Licensed (#126)
    * add license file
    
    * update license

--- docker/docker-entrypoint-local.sh
+++ docker/docker-entrypoint-local.sh
@@ -1,4 +1,15 @@
 #!/bin/bash -xe
+#
+# © 2017 Stratio Big Data Inc., Sucursal en España.
+#
+# This software is licensed under the Apache 2.0.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the terms of the License for more details.
+#
+# SPDX-License-Identifier:  Apache-2.0.
+#
+
 
 if [ $SEED = "true" ]; then
     java -jar -Dkhermes.ws=true -Dakka.remote.netty.tcp.port=$SEED_PORT -Dakka.remote.netty.tcp.hostname=localhost -Dakka.cluster.seed-nodes.0=akka.tcp://khermes@localhost:$SEED_PORT -Dmetrics.graphite.enabled=$METRICS_ENABLED -Dmetrics.graphite.name=$GRAPHITE_METRICS_NAME -Dzookeeper.connection=localhost:$ZK_PORT /khermes.jar
--- src/main/scala/com/stratio/khermes/metrics/KhermesMetrics.scala
+++ src/main/scala/com/stratio/khermes/metrics/KhermesMetrics.scala
@@ -1,19 +1,13 @@
-/*
- * Copyright (C) 2016 Stratio (http://stratio.com)
+/**
+ * © 2017 Stratio Big Data Inc., Sucursal en España.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This software is licensed under the Apache 2.0.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the terms of the License for more details.
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier:  Apache-2.0.
  */
-
 package com.stratio.khermes.metrics
 
 import com.stratio.khermes.commons.constants.AppConstants
